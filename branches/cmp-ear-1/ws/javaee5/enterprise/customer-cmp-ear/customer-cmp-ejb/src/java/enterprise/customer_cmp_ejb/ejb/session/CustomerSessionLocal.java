7f506568f74530a45d03ed95d74e69795360828d
nobody <nobody@7a5cb7d8-66fe-c9ed-9ef5-cf230e9c3e8b>
2006-04-06 20:33:55 +0000

    This commit was manufactured by cvs2svn to create branch 'cmp-ear-1'.

---
+++ branches/cmp-ear-1/ws/javaee5/enterprise/customer-cmp-ear/customer-cmp-ejb/src/java/enterprise/customer_cmp_ejb/ejb/session/CustomerSessionLocal.java
@@ -0,0 +1,48 @@
+/*
+ * CustomerSessionLocal.java
+ *
+ * Created on January 16, 2006, 6:43 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+/**
+ *
+ * @author Rahul Biswas
+ */
+
+package enterprise.customer_cmp_ejb.ejb.session;
+
+import javax.ejb.Local;
+import enterprise.customer_cmp_ejb.persistence.*;
+import enterprise.customer_cmp_ejb.common.*;
+
+import java.util.List;
+
+@Local
+public interface CustomerSessionLocal {
+    
+    public Customer searchForCustomer(String id);
+    
+    public Subscription searchForSubscription(String id);
+    
+    public Address searchForAddress(String id);
+    
+    public void persist(Object obj);
+    
+    public List findAllSubscriptions();
+    
+    public List findCustomerByFirstName(String firstName);
+    
+    public List findCustomerByLastName(String lastName);
+    
+    public void remove(Object obj);
+    
+    public Customer addCustomerAddress(Customer cust, Address address); 
+
+    public Customer removeCustomerSubscription(String cust, String subs) throws SubscriptionNotFoundException;
+    
+    public Customer addCustomerSubscription(String cust, String subs) throws DuplicateSubscriptionException;
+}
