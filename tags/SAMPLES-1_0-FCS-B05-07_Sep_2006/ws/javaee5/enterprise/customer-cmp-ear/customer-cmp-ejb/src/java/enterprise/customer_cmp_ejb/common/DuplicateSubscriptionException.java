11e0596008fcad373f0cb653dcd916c0244d710b
nobody <nobody@7a5cb7d8-66fe-c9ed-9ef5-cf230e9c3e8b>
2006-09-06 22:12:32 +0000

    This commit was manufactured by cvs2svn to create tag 'SAMPLES-1_0-FCS-B05-07_Sep_2006'.

---
+++ tags/SAMPLES-1_0-FCS-B05-07_Sep_2006/ws/javaee5/enterprise/customer-cmp-ear/customer-cmp-ejb/src/java/enterprise/customer_cmp_ejb/common/DuplicateSubscriptionException.java
@@ -0,0 +1,49 @@
+/*
+ * DuplicateSubscriptionException.java
+ *
+ * Created on March 17, 2006, 12:14 PM
+ *
+ * Copyright (c) 1999 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software
+ * and its documentation for NON-COMMERCIAL purposes and without
+ * fee is hereby granted provided that this copyright notice
+ * appears in all copies. Please refer to the file "LICENSE"
+ * for further important copyright and licensing information.
+ *
+ * SUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ * THIS SOFTWARE IS NOT DESIGNED OR INTENDED FOR USE OR RESALE AS ON-LINE
+ * CONTROL EQUIPMENT IN HAZARDOUS ENVIRONMENTS REQUIRING FAIL-SAFE
+ * PERFORMANCE, SUCH AS IN THE OPERATION OF NUCLEAR FACILITIES, AIRCRAFT
+ * NAVIGATION OR COMMUNICATION SYSTEMS, AIR TRAFFIC CONTROL, DIRECT LIFE
+ * SUPPORT MACHINES, OR WEAPONS SYSTEMS, IN WHICH THE FAILURE OF THE
+ * SOFTWARE COULD LEAD DIRECTLY TO DEATH, PERSONAL INJURY, OR SEVERE
+ * PHYSICAL OR ENVIRONMENTAL DAMAGE ("HIGH RISK ACTIVITIES").  SUN
+ * SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTY OF FITNESS FOR
+ * HIGH RISK ACTIVITIES.
+ */
+
+/**
+ *
+ * @author Rahul Biswas
+ */
+
+package enterprise.customer_cmp_ejb.common;
+
+public class DuplicateSubscriptionException extends Exception{
+    
+    /** Creates a new instance of DuplicateSubscriptionException */
+    public DuplicateSubscriptionException() {
+    }
+    
+    public DuplicateSubscriptionException(String message) {
+        super(message);
+    }
+    
+}
