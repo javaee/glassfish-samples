b6858e698b29972ec582b05b4e410b3aacbf2838
Roger Kitain <roger.kitain@oracle.com>
2009-07-30 17:56:59 +0000

    update to work with 1.0.0.PREVIEW2.SP2 of webbeans
    svn path=/trunk/; revision=550

--- ws/javaee6/webbeans/webbeans-guess/src/java/webbeansguess/Game.java
+++ ws/javaee6/webbeans/webbeans-guess/src/java/webbeansguess/Game.java
@@ -1,170 +1,113 @@
-/*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of either the GNU
- * General Public License Version 2 only ("GPL") or the Common Development
- * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License. You can obtain
- * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
- * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
- * language governing permissions and limitations under the License.
- *
- * When distributing the software, include this License Header Notice in each
- * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * Sun designates this particular file as subject to the "Classpath" exception
- * as provided by Sun in the GPL Version 2 section of the License file that
- * accompanied this code.  If applicable, add the following below the License
- * Header, with the fields enclosed by brackets [] replaced by your own
- * identifying information: "Portions Copyrighted [year]
- * [name of copyright owner]"
- *
- * Contributor(s):
- *
- * If you wish your version of this file to be governed by only the CDDL or
- * only the GPL Version 2, indicate your decision by adding "[Contributor]
- * elects to include this software in this distribution under the [CDDL or GPL
- * Version 2] license."  If you don't indicate a single choice of license, a
- * recipient has the option to distribute your version of this file under
- * either the CDDL, the GPL Version 2 or to extend the choice of license to
- * its licensees as provided above.  However, if you add GPL Version 2 code
- * and therefore, elected the GPL Version 2 license, then the option applies
- * only if the new code is made subject to such option by the copyright
- * holder.
- */
-
-package webbeansguess;
+package org.jboss.webbeans.examples.numberguess;
 
 
 import java.io.Serializable;
 
-import javax.annotation.Named;
 import javax.annotation.PostConstruct;
-import javax.context.Conversation;
-import javax.context.ConversationScoped;
-import javax.context.SessionScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.Named;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
-import javax.inject.AnnotationLiteral;
-import javax.inject.Current;
-import javax.inject.manager.Manager;
 
-/**
- * The Game class keeps track of Game information such as valid number ranges and 
- * number guesses.  Each game interaction is a Web Beans <code>conversation</code>.
- */
- 
 @Named
-@ConversationScoped
-public class Game implements Serializable {
-    private int number;
+@SessionScoped
+public class Game implements Serializable
+{
+   private int number;
    
-    private int guess;
-    private int smallest;
+   private int guess;
+   private int smallest;
    
-    @MaxNumber
-    private int maxNumber;
+   @MaxNumber
+   private int maxNumber;
    
-    private int biggest;
-    private int remainingGuesses;
+   private int biggest;
+   private int remainingGuesses;
    
-    @Current Manager manager;
+   @Random Instance<Integer> randomNumber;
+   
+   public Game()
+   {
+   }
 
-    private @Current Conversation conversation;
+   public int getNumber()
+   {
+      return number;
+   }
    
-    public Game() {
-    }
+   public int getGuess()
+   {
+      return guess;
+   }
+   
+   public void setGuess(int guess)
+   {
+      this.guess = guess;
+   }
+   
+   public int getSmallest()
+   {
+      return smallest;
+   }
+   
+   public int getBiggest()
+   {
+      return biggest;
+   }
+   
+   public int getRemainingGuesses()
+   {
+      return remainingGuesses;
+   }
+   
+   public String check() throws InterruptedException
+   {
+      if (guess>number)
+      {
+         biggest = guess - 1;
+      }
+      if (guess<number)
+      {
+         smallest = guess + 1;
+      }
+      if (guess == number)
+      {
+         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Correct!"));
+      }
+      remainingGuesses--;
+      return null;
+   }
+   
+   @PostConstruct
+   public void reset()
+   {
+      this.smallest = 0;
+      this.guess = 0;
+      this.remainingGuesses = 10;
+      this.biggest = maxNumber;
+      this.number = randomNumber.get();
+   }
+   
+   public void validateNumberRange(FacesContext context,  UIComponent toValidate, Object value)
+   {
+      if (remainingGuesses <= 0)
+      {
+         FacesMessage message = new FacesMessage("No guesses left!");
+         context.addMessage(toValidate.getClientId(context), message);
+         ((UIInput)toValidate).setValid(false);
+         return;
+      }
+      int input = (Integer) value;
 
-    public int getNumber() {
-        return number;
-    }
-   
-    /**
-     * Return the user's number guess.
-     */
-    public int getGuess() {
-        return guess;
-    }
-   
-    /**
-     * Store the user's number guess.
-     */
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-   
-    /**
-     * Return the low end of the number range of valid guesses.
-     */
-    public int getSmallest() {
-        return smallest;
-    }
-   
-    /**
-     * Return the high end of the number range of valid guesses.
-     */
-    public int getBiggest() {
-        return biggest;
-    }
-   
-    /**
-     * Determine how many guesses are left in the game.
-     */
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-   
-    /**
-     * Start a new <code>conversation</code> scope if necessary.
-     * Check the users's guess and recalculate new number ranges.
-     */
-    public String check() throws InterruptedException {
-        if (conversation.getId() == null) {
-            conversation.begin();
-        }
+      if (input < smallest || input > biggest) 
+	   {
+         ((UIInput)toValidate).setValid(false);
 
-        if (guess>number) {
-            biggest = guess - 1;
-        }
-        if (guess<number) {
-            smallest = guess + 1;
-        }
-        if (guess == number) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Correct!"));
-        }
-        remainingGuesses--;
-        return null;
-    }
-   
-    /**
-     * Reset the game.  This will end the current <code>conversation</code> scope and
-     * start a new one.
-     */
-    @PostConstruct
-    public void reset() {
-        this.smallest = 0;
-        this.guess = 0;
-        this.remainingGuesses = 10;
-        this.biggest = maxNumber;
-        this.number = manager.getInstanceByType(Integer.class, new AnnotationLiteral<Random>(){});
-        conversation.end();
-        conversation.begin();
-    }
-   
-    /**
-     * Validate a number value within a number range.
-     */
-    public void validateNumberRange(FacesContext context,  UIComponent toValidate, Object value) {
-        int input = (Integer) value;
-
-        if (input < smallest || input > biggest) {
-            ((UIInput)toValidate).setValid(false);
-
-            FacesMessage message = new FacesMessage("Invalid guess");
-            context.addMessage(toValidate.getClientId(context), message);
-        }
-    }
+         FacesMessage message = new FacesMessage("Invalid guess");
+         context.addMessage(toValidate.getClientId(context), message);
+      }
+   }
 }
