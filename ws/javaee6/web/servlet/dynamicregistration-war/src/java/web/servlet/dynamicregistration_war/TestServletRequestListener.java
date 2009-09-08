e53a015b91f8a6ee05b376b84785983f4b80dc82
jluehe <jluehe@7a5cb7d8-66fe-c9ed-9ef5-cf230e9c3e8b>
2009-09-08 17:45:25 +0000

    Expanded sample by adding dynamic registration of ServletRequestListener

--- ws/javaee6/web/servlet/dynamicregistration-war/src/java/web/servlet/dynamicregistration_war/TestServletRequestListener.java
+++ ws/javaee6/web/servlet/dynamicregistration-war/src/java/web/servlet/dynamicregistration_war/TestServletRequestListener.java
@@ -36,50 +36,29 @@
 
 package web.servlet.dynamicregistration_war;
 
-import java.util.*;
 import javax.servlet.*;
 
-/**
- * ServletContextListener that registers a Servlet (with
- * name <tt>DynamicServlet</tt>) and Filter (with name 
- * <tt>DynamicFilter</tt>) in response to the <tt>contextInitialized</tt>
- * event.
- *
- * <p>The <tt>DynamicServlet</tt> is configured with an initialization
- * parameter and mapped to a URL pattern equal to <tt>/*</tt>.
- *
- * <p>The <tt>DynamicFilter</tt> is also configured with an initialization
- * parameter and mapped to the <tt>DynamicServlet</tt> such that it will
- * intercept any requests mapped to the <tt>DynamicServlet</tt>.
- *
- * @author Jan Luehe
- */
-public class TestServletContextListener implements ServletContextListener {
+public class TestServletRequestListener implements ServletRequestListener {
 
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-
-        ServletContext sc = sce.getServletContext();
-
-        // Register Servlet
-        ServletRegistration sr = sc.addServlet("DynamicServlet",
-            "web.servlet.dynamicregistration_war.TestServlet");
-        sr.setInitParameter("servletInitName", "servletInitValue");
-        sr.addMapping("/*");
-
-        // Register Filter
-        FilterRegistration fr = sc.addFilter("DynamicFilter",
-            "web.servlet.dynamicregistration_war.TestFilter");
-        fr.setInitParameter("filterInitName", "filterInitValue");
-        fr.addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST),
-                                     true, "DynamicServlet");
-
-        // Register ServletRequestListener
-        sc.addListener("web.servlet.dynamicregistration_war.TestServletRequestListener");
-    }   
-
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {  
+    /**
+     * Receives notification that a request is about to enter the scope
+     * of the web application.
+     *
+     * @param sre The ServletRequestEvent
+     */
+    public void requestInitialized(ServletRequestEvent sre) {
+        sre.getServletRequest().setAttribute("listenerAttributeName",
+            "listenerAttributeValue");
+    }
+    
+    /**
+     * Receives notification that a request is about to leave the scope
+     * of the web application.
+     *
+     * @param sre The ServletRequestEvent
+     */
+    public void requestDestroyed(ServletRequestEvent sre) {
         // Do nothing
     }
+
 }
