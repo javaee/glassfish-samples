ab4cb55aace68f32070f68d3b78979bd8e1f5d4a
pavel_bucek <pavel_bucek@7a5cb7d8-66fe-c9ed-9ef5-cf230e9c3e8b>
2009-10-18 04:32:32 +0000

    better way how construct link to individual message

--- ws/javaee6/rest/message-board-war/src/java/enterprise/messageboard/entities/MessageListWriter.java
+++ ws/javaee6/rest/message-board-war/src/java/enterprise/messageboard/entities/MessageListWriter.java
@@ -42,17 +42,22 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.net.URI;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
 @Stateless
 @Provider
 public class MessageListWriter implements MessageBodyWriter<List<Message>> {
+
+    @Context UriInfo ui;
 
     public boolean isWriteable(Class<?> clazz, Type type, Annotation[] annotation, MediaType mediaType) {
         return verifyGenericType(type);
@@ -78,7 +83,8 @@ public class MessageListWriter implements MessageBodyWriter<List<Message>> {
     public void writeTo(List<Message> messages, Class<?> clazz, Type type, Annotation[] annotation, MediaType mediaType, MultivaluedMap<String, Object> arg5, OutputStream ostream) throws IOException, WebApplicationException {
         for (Message m : messages) {
             ostream.write(m.toString().getBytes());
-            ostream.write((" <a href='./app/messages/" + m.getUniqueId() + "'>link</a><br />").getBytes());
+            URI mUri = ui.getAbsolutePathBuilder().path(Integer.toString(m.getUniqueId())).build();
+            ostream.write((" <a href='" + mUri.toASCIIString() + "'>link</a><br />").getBytes());
         }
     }
 }
