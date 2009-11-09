3662970ccf1e5aee2060b085bb8e5d079601a8dd
driscoll <driscoll@7a5cb7d8-66fe-c9ed-9ef5-cf230e9c3e8b>
2009-11-09 04:49:21 +0000

    Update JSF basic-ezcomp demo to remove all Netbeans errors.

--- ws/javaee6/web/jsf/basic-ezcomp/src/java/ezcomp/MultiNumberHolder.java
+++ ws/javaee6/web/jsf/basic-ezcomp/src/java/ezcomp/MultiNumberHolder.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -38,10 +38,13 @@ package ezcomp;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 
 @ManagedBean(name="multinumber")
 @SessionScoped
-public class MultiNumberHolder {
+public class MultiNumberHolder implements Serializable {
+
+    private static final long serialVersionUID = 6733229187968502934L;
 
     private Integer i1 = 1;
     private Integer i2 = 2;
