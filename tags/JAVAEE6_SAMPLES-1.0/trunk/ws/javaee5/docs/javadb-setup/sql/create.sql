4e1df40a8b728616ad4fc6565bc04591284b038c
janey <janey@7a5cb7d8-66fe-c9ed-9ef5-cf230e9c3e8b>
2011-08-15 22:56:14 +0000

    remove tag...

--- tags/JAVAEE6_SAMPLES-1.0/trunk/ws/javaee5/docs/javadb-setup/sql/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE customer (
- customerid VARCHAR(8) NOT NULL,
- lastname VARCHAR(24) NOT NULL,
- firstname VARCHAR(24) NOT NULL,
- primary key (customerid)
-);
-
-INSERT INTO customer VALUES('SUN00001', 'LastName', 'FirstName');
