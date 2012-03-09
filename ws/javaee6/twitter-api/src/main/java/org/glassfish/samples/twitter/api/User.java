ed49ae8ddb736f02834a35bc911eb71331be0f12
Arun Gupta <arungupta@java.net>
2012-03-09 01:13:34 +0000

    Moving from branch "arun" to glassfish-samples. These will be committed to maven central with org.glassfish.samples groupId.
    svn path=/trunk/; revision=834

---
+++ ws/javaee6/twitter-api/src/main/java/org/glassfish/samples/twitter/api/User.java
@@ -0,0 +1,419 @@
+/*
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *  Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ *  The contents of this file are subject to the terms of either the GNU
+ *  General Public License Version 2 only ("GPL") or the Common Development
+ *  and Distribution License("CDDL") (collectively, the "License").  You
+ *  may not use this file except in compliance with the License.  You can
+ *  obtain a copy of the License at
+ *  https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ *  or packager/legal/LICENSE.txt.  See the License for the specific
+ *  language governing permissions and limitations under the License.
+ *
+ *  When distributing the software, include this License Header Notice in each
+ *  file and include the License file at packager/legal/LICENSE.txt.
+ *
+ *  GPL Classpath Exception:
+ *  Oracle designates this particular file as subject to the "Classpath"
+ *  exception as provided by Oracle in the GPL Version 2 section of the License
+ *  file that accompanied this code.
+ *
+ *  Modifications:
+ *  If applicable, add the following below the License Header, with the fields
+ *  enclosed by brackets [] replaced by your own identifying information:
+ *   "Portions Copyright [year] [name of copyright owner]"
+ *
+ *  Contributor(s):
+ *  If you wish your version of this file to be governed by only the CDDL or
+ *  only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *  elects to include this software in this distribution under the [CDDL or GPL
+ *  Version 2] license."  If you don't indicate a single choice of license, a
+ *  recipient has the option to distribute your version of this file under
+ *  either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *  its licensees as provided above.  However, if you add GPL Version 2 code
+ *  and therefore, elected the GPL Version 2 license, then the option applies
+ *  only if the new code is made subject to such option by the copyright
+ *  holder.
+ */
+package org.glassfish.samples.twitter.api;
+
+import javax.inject.Named;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Arun Gupta
+ */
+@XmlRootElement
+@Named
+public class User {
+    private boolean contributors_enabled;
+    private String lang;
+    private String profile_background_image_url;
+    private boolean protected2;
+    private String profile_link_color;
+    private String url;
+    private String name;
+    private int listed_count;
+    private int utc_offset;
+    private String profile_background_color;
+    private long followers_count;
+    private String profile_image_url;
+    private String description;
+    private boolean profile_background_tile;
+    private boolean default_profile;
+    private long statuses_count;
+    private boolean following;
+    private String created_at;
+    private String profile_sidebar_fill_color;
+    private String screen_name;
+    private boolean geo_enabled;
+    private boolean default_profile_image;
+    private long friends_count;
+    private String profile_sidebar_border_color;
+    private String id_str;
+    private boolean show_all_inline_media;
+    private boolean follow_request_sent;
+    private String profile_background_image_url_https;
+    private boolean is_translator;
+    private boolean notifications;
+    private boolean profile_use_background_image;
+    private String profile_image_url_https;
+    private long id;
+    private boolean verified;
+    private String time_zone;
+    private long favourites_count;
+    private String profile_text_color;
+    private String location;
+    private Tweet status;
+    private String profile_banner_url;
+
+    public boolean isContributors_enabled() {
+        return contributors_enabled;
+    }
+
+    public void setContributors_enabled(boolean contributors_enabled) {
+        this.contributors_enabled = contributors_enabled;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public boolean isDefault_profile() {
+        return default_profile;
+    }
+
+    public void setDefault_profile(boolean default_profile) {
+        this.default_profile = default_profile;
+    }
+
+    public boolean isDefault_profile_image() {
+        return default_profile_image;
+    }
+
+    public void setDefault_profile_image(boolean default_profile_image) {
+        this.default_profile_image = default_profile_image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getFavourites_count() {
+        return favourites_count;
+    }
+
+    public void setFavourites_count(long favourites_count) {
+        this.favourites_count = favourites_count;
+    }
+
+    public boolean isFollow_request_sent() {
+        return follow_request_sent;
+    }
+
+    public void setFollow_request_sent(boolean follow_request_sent) {
+        this.follow_request_sent = follow_request_sent;
+    }
+
+    public long getFollowers_count() {
+        return followers_count;
+    }
+
+    public void setFollowers_count(long followers_count) {
+        this.followers_count = followers_count;
+    }
+
+    public boolean isFollowing() {
+        return following;
+    }
+
+    public void setFollowing(boolean following) {
+        this.following = following;
+    }
+
+    public long getFriends_count() {
+        return friends_count;
+    }
+
+    public void setFriends_count(long friends_count) {
+        this.friends_count = friends_count;
+    }
+
+    public boolean isGeo_enabled() {
+        return geo_enabled;
+    }
+
+    public void setGeo_enabled(boolean geo_enabled) {
+        this.geo_enabled = geo_enabled;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getId_str() {
+        return id_str;
+    }
+
+    public void setId_str(String id_str) {
+        this.id_str = id_str;
+    }
+
+    public boolean isIs_translator() {
+        return is_translator;
+    }
+
+    public void setIs_translator(boolean is_translator) {
+        this.is_translator = is_translator;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public int getListed_count() {
+        return listed_count;
+    }
+
+    public void setListed_count(int listed_count) {
+        this.listed_count = listed_count;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(boolean notifications) {
+        this.notifications = notifications;
+    }
+
+    public String getProfile_background_color() {
+        return profile_background_color;
+    }
+
+    public void setProfile_background_color(String profile_background_color) {
+        this.profile_background_color = profile_background_color;
+    }
+
+    public String getProfile_background_image_url() {
+        return profile_background_image_url;
+    }
+
+    public void setProfile_background_image_url(String profile_background_image_url) {
+        this.profile_background_image_url = profile_background_image_url;
+    }
+
+    public String getProfile_background_image_url_https() {
+        return profile_background_image_url_https;
+    }
+
+    public void setProfile_background_image_url_https(String profile_background_image_url_https) {
+        this.profile_background_image_url_https = profile_background_image_url_https;
+    }
+
+    public boolean isProfile_background_tile() {
+        return profile_background_tile;
+    }
+
+    public void setProfile_background_tile(boolean profile_background_tile) {
+        this.profile_background_tile = profile_background_tile;
+    }
+
+    public String getProfile_image_url() {
+        return profile_image_url;
+    }
+
+    public void setProfile_image_url(String profile_image_url) {
+        this.profile_image_url = profile_image_url;
+    }
+
+    public String getProfile_image_url_https() {
+        return profile_image_url_https;
+    }
+
+    public void setProfile_image_url_https(String profile_image_url_https) {
+        this.profile_image_url_https = profile_image_url_https;
+    }
+
+    public String getProfile_link_color() {
+        return profile_link_color;
+    }
+
+    public void setProfile_link_color(String profile_link_color) {
+        this.profile_link_color = profile_link_color;
+    }
+
+    public String getProfile_sidebar_border_color() {
+        return profile_sidebar_border_color;
+    }
+
+    public void setProfile_sidebar_border_color(String profile_sidebar_border_color) {
+        this.profile_sidebar_border_color = profile_sidebar_border_color;
+    }
+
+    public String getProfile_sidebar_fill_color() {
+        return profile_sidebar_fill_color;
+    }
+
+    public void setProfile_sidebar_fill_color(String profile_sidebar_fill_color) {
+        this.profile_sidebar_fill_color = profile_sidebar_fill_color;
+    }
+
+    public String getProfile_text_color() {
+        return profile_text_color;
+    }
+
+    public void setProfile_text_color(String profile_text_color) {
+        this.profile_text_color = profile_text_color;
+    }
+
+    public boolean isProfile_use_background_image() {
+        return profile_use_background_image;
+    }
+
+    public void setProfile_use_background_image(boolean profile_use_background_image) {
+        this.profile_use_background_image = profile_use_background_image;
+    }
+
+    @XmlElement(name="protected")
+    public boolean isProtected2() {
+        return protected2;
+    }
+
+    public void setProtected2(boolean protected2) {
+        this.protected2 = protected2;
+    }
+
+    public String getScreen_name() {
+        return screen_name;
+    }
+
+    public void setScreen_name(String screen_name) {
+        this.screen_name = screen_name;
+    }
+
+    public boolean isShow_all_inline_media() {
+        return show_all_inline_media;
+    }
+
+    public void setShow_all_inline_media(boolean show_all_inline_media) {
+        this.show_all_inline_media = show_all_inline_media;
+    }
+
+    public long getStatuses_count() {
+        return statuses_count;
+    }
+
+    public void setStatuses_count(long statuses_count) {
+        this.statuses_count = statuses_count;
+    }
+
+    public String getTime_zone() {
+        return time_zone;
+    }
+
+    public void setTime_zone(String time_zone) {
+        this.time_zone = time_zone;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getUtc_offset() {
+        return utc_offset;
+    }
+
+    public void setUtc_offset(int utc_offset) {
+        this.utc_offset = utc_offset;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    @XmlElement(nillable=true)
+    public Tweet getStatus() {
+        return status;
+    }
+
+    public void setStatus(Tweet status) {
+        this.status = status;
+    }
+
+    @XmlElement(nillable=true)
+    public String getProfile_banner_url() {
+        return profile_banner_url;
+    }
+
+    public void setProfile_banner_url(String profile_banner_url) {
+        this.profile_banner_url = profile_banner_url;
+    }
+    
+    public String toString() {
+        return name;
+    }
+
+}
