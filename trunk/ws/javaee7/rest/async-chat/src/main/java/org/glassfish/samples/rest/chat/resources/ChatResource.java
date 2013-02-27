fe0191c23fd9da74dabc647cc09a7077f89cf6b2
mfuksa <mfuksa@7a5cb7d8-66fe-c9ed-9ef5-cf230e9c3e8b>
2013-02-27 19:34:46 +0000

    jax-rs rest samples: Deleted managed-client sample (jax-rs has no longer @Uri) + small changes in other samples.

--- trunk/ws/javaee7/rest/async-chat/src/main/java/org/glassfish/samples/rest/chat/resources/ChatResource.java
+++ trunk/ws/javaee7/rest/async-chat/src/main/java/org/glassfish/samples/rest/chat/resources/ChatResource.java
@@ -43,8 +43,10 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Response;
@@ -56,7 +58,9 @@ import javax.ejb.Singleton;
  * Resource that handles one way asynchronous chatting based on consumes produces mechanism.
  * <p/>
  * Message is sent (produces) using POST http method handled by {@link #postMessage(javax.ws.rs.container.AsyncResponse, String)}
- * and this message is received (consumed) by GET http method handled by {@link #getMessage(javax.ws.rs.container.AsyncResponse)}.
+ * and this message is received (consumed) by GET http method handled by {@link #getMessage(javax.ws.rs.container.AsyncResponse,
+ * String)}.
+ * <p/>
  * This implementation of produces consumes mechanism does not keep a queue of messages but a queue of responses waiting for
  * messages. Message is always delivered directly from thread handling posting of message to the thread handling receiving of
  * message. Request processing must be therefore synchronized by blocking threads. In order to save server resources the original
@@ -73,15 +77,39 @@ public class ChatResource {
      * This queue synchronizes produces/consumes operations. It contains {@link AsyncResponse async responses} into
      * which post message should be written.
      */
-    private final BlockingQueue<AsyncResponse> suspended = new ArrayBlockingQueue<AsyncResponse>(10);
+    private final BlockingQueue<AsyncResponseWrapper> suspended = new ArrayBlockingQueue<AsyncResponseWrapper>(10);
+
+    /**
+     * Internal response wrapper which bundles response with id.
+     */
+    private static class AsyncResponseWrapper {
+        private final AsyncResponse asyncResponse;
+        private final String id;
+
+        private AsyncResponseWrapper(AsyncResponse asyncResponse, String id) {
+            this.asyncResponse = asyncResponse;
+            this.id = id;
+        }
+
+        public AsyncResponse getAsyncResponse() {
+            return asyncResponse;
+        }
+
+        public String getId() {
+            return id;
+        }
+    }
+
 
     /**
      * Handle a HTTP get message asynchronously (suspend response in order to release the container thread instead of
      * blocking it on the blocking queue).
+     *
      * @param asyncResponse Suspended asynchronous response (injected).
+     * @param requestId Header identifying the header.
      */
     @GET
-    public void getMessage(@Suspended final AsyncResponse asyncResponse) {
+    public void getMessage(@Suspended final AsyncResponse asyncResponse, final @HeaderParam("request-id") String requestId) {
         new Thread(new Runnable() {
 
             @Override
@@ -89,7 +117,7 @@ public class ChatResource {
                 try {
                     // Put actual response to the queue. This response will be later taken and resumed with
                     // the message.
-                    suspended.put(asyncResponse);
+                    suspended.put(new AsyncResponseWrapper(asyncResponse, requestId));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -114,8 +142,9 @@ public class ChatResource {
                 try {
                     // Take one response from the queue and resume it with the message. If no message is in the queue now
                     // then this method will block the thread until the response is put into queue (by GET http method).
-                    final AsyncResponse asyncResponse = suspended.take();
-                    asyncResponse.resume(Response.ok().entity(message).build());
+                    final AsyncResponseWrapper responseWrapper = suspended.take();
+                    responseWrapper.getAsyncResponse().resume(Response.ok()
+                            .entity(message).header("request-id", responseWrapper.getId()).build());
 
                     // Now resume response connected with the request invoking this post method just reply that the message
                     // was delivered.
@@ -125,5 +154,29 @@ public class ChatResource {
                 }
             }
         }).start();
+    }
+
+    /**
+     * Get the string representation of internal async response queue <code>suspended<code/>.
+     * <p/>
+     * This resource is requested in regular intervals by clients.
+     *
+     * @return Plain text with list of request-id of async responses.
+     */
+    @GET
+    @Path("queue")
+    @Produces("text/html")
+    public String getResponseQueue() {
+        StringBuffer sb = new StringBuffer();
+        boolean addSeparator = false;
+        for (AsyncResponseWrapper asyncResponseWrapper : suspended) {
+            if (addSeparator) {
+                sb.append(", ");
+            } else {
+                addSeparator = true;
+            }
+            sb.append(asyncResponseWrapper.getId());
+        }
+        return sb.toString();
     }
 }
