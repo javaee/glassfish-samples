e21e6048d5f7028dbbd04099a523ba1f52f00bb0
Shing Wai Chan <shing.wai.chan@oracle.com>
2017-08-28 12:24:06 -0700

    add http trailer sample

---
+++ ws/javaee8/servlet/trailer-war/src/main/java/org/glassfish/servlet/trailer_war/TestServlet.java
@@ -0,0 +1,103 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.servlet.push_war;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This class illustrates HTTP trailer API in Servlet.
+ *
+ * @author Shing Wai Chan
+ */
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        res.setContentType("text/plain");
+        res.addHeader("Transfer-encoding", "chunked");
+        res.addHeader("TE", "trailers");
+        res.addHeader("Trailer", "bar");
+
+        StringBuilder sb = new StringBuilder();
+
+        final InputStream in = req.getInputStream();
+        int b;
+        while ((b = in.read()) != -1) {
+            sb.append((char) b);
+        }
+
+        String foo = null;
+        int size = -1;
+
+        if (req.isTrailerFieldsReady()) {
+            Map<String, String> reqTrailerFields = req.getTrailerFields();
+            size = reqTrailerFields.size();
+            foo = reqTrailerFields.get("foo");
+        }
+
+        final String finalFoo = foo;
+        final int finalSize = size;
+        res.setTrailerFields(new Supplier<Map<String, String>>() {
+            @Override
+            public Map<String, String> get() {
+                Map<String, String> map = new HashMap<>();
+                map.put("bar", finalFoo + finalSize);
+                return map;
+            }
+        });
+        res.getWriter().write(sb.toString());
+    }
+}
