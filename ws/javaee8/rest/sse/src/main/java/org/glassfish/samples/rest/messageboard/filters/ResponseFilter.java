6035b5f38abe2f7e635e2f480db6e9f2d8863547
Ed Burns <edward.burns@oracle.com>
2017-10-16 16:01:28 -0400

    In preparation for prototyping, get a simple clean example of JAX-RS 2.1 SSE
    new file:   ws/javaee8/rest/sse/docs/index.html
    new file:   ws/javaee8/rest/sse/pom.xml
    new file:   ws/javaee8/rest/sse/src/main/java/org/glassfish/samples/rest/messageboard/exceptions/NotFoundException.java
    new file:   ws/javaee8/rest/sse/src/main/java/org/glassfish/samples/rest/messageboard/exceptions/NotFoundExceptionMapper.java
    new file:   ws/javaee8/rest/sse/src/main/java/org/glassfish/samples/rest/messageboard/filters/ResponseFilter.java
    new file:   ws/javaee8/rest/sse/src/main/java/org/glassfish/samples/rest/messageboard/resources/JaxRsApplication.java
    new file:   ws/javaee8/rest/sse/src/main/java/org/glassfish/samples/rest/messageboard/resources/SSERootResource.java
    new file:   ws/javaee8/rest/sse/src/main/webapp/WEB-INF/glassfish-web.xml
    new file:   ws/javaee8/rest/sse/src/main/webapp/WEB-INF/web.xml
    new file:   ws/javaee8/rest/sse/src/main/webapp/index.html
    new file:   ws/javaee8/rest/sse/src/main/webapp/sse-simple.html
    modified:   .gitignore
    modified:   ws/javaee8/docs/list.html
    modified:   ws/javaee8/rest/pom.xml

---
+++ ws/javaee8/rest/sse/src/main/java/org/glassfish/samples/rest/messageboard/filters/ResponseFilter.java
@@ -0,0 +1,68 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://oss.oracle.com/licenses/CDDL+GPL-1.1
+ * or LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.samples.rest.messageboard.filters;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
+/**
+ * {@link ContainerResponseFilter Container response fitler} that adds headers to the response.
+ * @author Miroslav Fuksa (miroslav.fuksa at oracle.com)
+ */
+public class ResponseFilter implements ContainerResponseFilter {
+    private final AtomicReference<Date> date = new AtomicReference<Date>();
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext,
+                       ContainerResponseContext containerResponseContext) throws IOException {
+
+        Date currentDate = new Date();
+        final Date lastDate = date.getAndSet(currentDate);
+
+        containerResponseContext.getHeaders().add("previous-response", lastDate == null ? "this is the first response"
+                : lastDate.toString());
+        containerResponseContext.getHeaders().add("this-response", currentDate.toString());
+    }
+}
